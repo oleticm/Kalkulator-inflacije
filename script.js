303760127a2bff639a84d147c3501b291f3222e0
oleticm <oleticmartin95@gmail.com>
2022-10-07 18:38:37 +0200

    Final commit

---
+++ script.js
@@ -0,0 +1,24 @@
+function inflationCalculator(){
+    let stopaInflacije = document.querySelector('#stopaInflacije');
+    let novac = document.querySelector('#novac');
+    
+    stopaInflacije = parseFloat(stopaInflacije.value);
+    novac = parseFloat(novac.value);
+
+    let godine = document.querySelector('#godine');
+    godine = parseFloat(godine.value);
+
+    let worth = novac + (novac * (stopaInflacije / 100));
+
+
+    for(let i = 1; i < godine; i++){
+        worth += worth * (stopaInflacije / 100);
+    }
+
+    worth= worth.toFixed(2);
+    
+    let newElement = document.createElement('div');
+    newElement.className = 'new-value';
+    newElement.innerText = `Današnjih ${novac}€ vrijedi isto kao ${worth}€ za ${godine} godina!`;
+    document.querySelector('.container').appendChild(newElement);
+}
